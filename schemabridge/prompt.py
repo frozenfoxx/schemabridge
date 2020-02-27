e1a4ef8dbc721a52f7f0e5f5ea3a92cb3dab8b7d
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2020-02-26 22:49:41 -0800

    Added planet id targeting

--- schemabridge/prompt.py
+++ schemabridge/prompt.py
@@ -28,11 +28,6 @@ class Prompt(Cmd, object):
         self.game = Game(self.cur)
         self.armada = Armada(self.cur)
 
-    def do_run(self, args):
-        """ Run the automated bot with or without a data seed """
-
-        print("[+] Run not yet implemented")
-
     def do_attack_all(self, args):
         """ Order all ships in range of enemy ships to attack """
 
@@ -53,8 +48,8 @@ class Prompt(Cmd, object):
         self.myplayer.update()
 
         print("[+] User      | Balance | Fuel Reserve")
-        print("[+] ----------------------------------")
-        print("[+] " + str(self.myplayer.username) + " | " + \
+        print("    ----------------------------------")
+        print("    " + str(self.myplayer.username) + " | " + \
             str(self.myplayer.balance) + " | " + \
             str(self.myplayer.fuel_reserve))
 
@@ -68,9 +63,9 @@ class Prompt(Cmd, object):
             print("[+] Done")
 
         print("[+] ID    | Name   | Mine Limit | Loc X | Loc Y | Conqueror")
-        print("[+] -------------------------------------------------------")
+        print("    -------------------------------------------------------")
         for i in self.game.planets:
-            print("[+] " + str(i.planet_id) + " | " + \
+            print("    " + str(i.planet_id) + " | " + \
                 str(i.name) + " | " + \
                 str(i.mine_limit) + " | " + \
                 str(i.location_x) + " | " + \
@@ -82,8 +77,8 @@ class Prompt(Cmd, object):
 
         planets = self.armada.planets_in_range()
 
-        print("[+] Ship ID | Planet(s)")
-        print("[+] -------------------")
+        print("[+] Ship ID | Planet ID")
+        print("    -------------------")
         for p in planets:
             print("[+] " + str(p) + " | " + str(planets[p]))
 
@@ -99,9 +94,9 @@ class Prompt(Cmd, object):
         self.game.update_players()
 
         print("[+] ID | Username   | Damage Taken | Damage Done | Planets Conq | Planets Lost | Ships Built | Ships Lost | Ship Upgrades | Distance | Fuel Mined")
-        print("[+] ---------------------------------------------------------------------------------------------------------------------------------------------")
+        print("    ---------------------------------------------------------------------------------------------------------------------------------------------")
         for i in self.game.players:
-            print("[+] " + str(i.player_id) + " | " + \
+            print("    " + str(i.player_id) + " | " + \
                 str(i.username) + " | " + \
                 str(i.damage_taken) + " | " + \
                 str(i.damage_done) + " | " + \
@@ -132,11 +127,13 @@ class Prompt(Cmd, object):
     def do_ship_create(self, args):
         """ Create a ship
             ship_create [name]
+            ship_create [name] [attack] [defense] [engineering] [prospecting] [planet id]
             ship_create [name] [attack] [defense] [engineering] [prospecting] [location_x] [location_y] """
 
         # Create a list from the argument string
         arg_list = args.split(' ')
         
+        # Check the arguments
         if len(args) == 0:
             print("[-] Error: argument list cannot be empty")
         elif len(arg_list) > 7:
@@ -147,20 +144,28 @@ class Prompt(Cmd, object):
 
             # Set ship attrs and location if offered
             stats = []
-            if len(arg_list) > 1:
-                stats.append(arg_list[1])
-                stats.append(arg_list[2])
-                stats.append(arg_list[3])
-                stats.append(arg_list[4])
-                location = arg_list[5] + "," + arg_list[6]
-                self.armada.create(name, stats, location)
-            else:
+            if len(arg_list) == 1:
                 stats.append(5)
                 stats.append(5)
                 stats.append(5)
                 stats.append(5)
                 location = ""
-                self.armada.create(name, stats, location)
+            else:
+                stats.append(arg_list[1])
+                stats.append(arg_list[2])
+                stats.append(arg_list[3])
+                stats.append(arg_list[4])
+                if len(arg_list) == 6:
+                    # Get coordinates from planet id
+                    for planet in self.game.planets:
+                        if arg_list[5] == str(planet.planet_id):
+                            location = str(planet.location_x) + "," + str(planet.location_y)
+                else:
+                    # Get coordinates from args
+                    location = arg_list[5] + "," + arg_list[6]
+
+            # Create the ship
+            self.armada.create(name, stats, location)
 
     def do_ship_mine(self, args):
         """ Order a ship in range of a planet to mine
@@ -180,6 +185,7 @@ class Prompt(Cmd, object):
 
     def do_ship_move(self, args):
         """ Move a ship at a speed towards a point
+            ship_move [ship id] [max|half] [planet id]
             ship_move [ship id] [max|half] [location_x] [location_y] """
 
         # Create a list from the argument string
@@ -188,11 +194,21 @@ class Prompt(Cmd, object):
         # Check argument list
         if len(args) == 0:
             print("[-] Error: argument list cannot be empty")
-        elif len(arg_list) != 4:
-            print("[-] Error: argument list incorrect")
+        elif len(arg_list) > 4:
+            print("[-] Error: argument list too long")
         else:
-            self.armada.move(arg_list[0], arg_list[1], arg_list[2], arg_list[3])
-            print("[+] Ship with id " + str(arg_list[0]) + " moved to POINT(" + str(arg_list[2]) + "," + str(arg_list[3]) + ")")
+            if len(arg_list) == 3:
+                # Get coordinates from planet id
+                for planet in self.game.planets:
+                    if arg_list[2] == str(planet.planet_id):
+                        location_x = str(planet.location_x)
+                        location_y = str(planet.location_y)
+            else:
+                # Get coordinates from args
+                location_x = arg_list[2]
+                location_y = arg_list[3]
+            self.armada.move(arg_list[0], arg_list[1], location_x, location_y)
+            print("[+] Ship with id " + str(arg_list[0]) + " moved to POINT(" + location_x + "," + location_y + ")")
 
     def do_ship_repair(self, args):
         """ Order a ship in range of a target ship to repair
@@ -234,10 +250,10 @@ class Prompt(Cmd, object):
 
         self.armada.update()
 
-        print("[+] id | fleet_id | player_id | name | last_action_tic | last_move_tic | last_living_tic | current_health | max_health | current_fuel | max_fuel | max_speed | range | attack | defense | engineering | prospecting | location_x | location_y | direction | speed | destination_x | destination_y | repair_priority | action | action_target_id |      location      | destination | target_speed | target_direction")
-        print("[+] -------------------")
+        print("[+] ID | fleet_id | player_id | name | last_action_tic | last_move_tic | last_living_tic | current_health | max_health | current_fuel | max_fuel | max_speed | range | attack | defense | engineering | prospecting | location_x | location_y | direction | speed | destination_x | destination_y | repair_priority | action | action_target_id |      location      | destination | target_speed | target_direction")
+        print("    -------------------")
         for ship in self.armada.ships:
-            print("[+] " + str(ship.ship_id) + " | " + \
+            print("    " + str(ship.ship_id) + " | " + \
                 str(ship.fleet_id) + " | " + \
                 str(ship.player_id) + " | " + \
                 str(ship.name) + " | " + \
@@ -274,9 +290,9 @@ class Prompt(Cmd, object):
         ships = self.armada.ships_in_range()
 
         print("[+] Ship ID | Target Ship ID(s)")
-        print("[+] ---------------------------")
+        print("    ---------------------------")
         for s in ships:
-            print("[+] " + str(s) + " | " + str(ships[s]))
+            print("    " + str(s) + " | " + str(ships[s]))
 
     def do_tic(self, args):
         """ Get current server Tic """
@@ -284,19 +300,14 @@ class Prompt(Cmd, object):
         self.game.update_tic()
         print("[+] Current Tic: " + str(self.game.tic))
 
-    def do_train(self, args):
-        """ Train the automated player """
-
-        print("[+] Training not yet implemented")
-
     def do_variables(self, args):
         """ Update and dump variables """
 
         self.game.update_variables()
         print("[+] Name | Value")
-        print("[+] ------------")
+        print("    ------------")
         for key, value in self.game.variables.items():
-            print("[+] " + str(key) + "|" + str(value))
+            print("    " + str(key) + "|" + str(value))
 
     def do_quit(self, args):
         """ Quit the program """
